b6d8cd1096b2f1952c2c216956fcf92663a6966b
Hoàng Văn Ngà <ngahv2222@gmail.com>
2020-02-03 13:57:26 +0700

    Integrate GitHub Action

--- graphql/resolves/auth.js
+++ graphql/resolves/auth.js
@@ -16,8 +16,20 @@ module.exports = {
       });
       const result = await newUser.save()
       return { ...result._doc, _id: result.id, password: null };
-    } catch (err) {
-      throw err;
+    } catch (error) {
+      throw error;
     }
   },
+  login: async args => {
+    try {
+      const user = await User.findOne({ email: args.email });
+      if (user) {
+        throw new Error('User exists already');
+      }
+      console.log("User is not created");
+
+    } catch (error) {
+      throw error;
+    }
+  }
 }
